13df383ca27b4cd79faa98fa8b8c2f120751078a
Volker Thiel <volker.thiel@mage-profis.de>
2016-05-27 15:26:45 +0200

    Added modman file, changed tabs to spaces

--- app/code/community/Agere/Wysiwyg/Model/Observer.php
+++ app/code/community/Agere/Wysiwyg/Model/Observer.php
@@ -8,15 +8,15 @@
  * @license http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  * @datetime: 20.04.14 15:02
  */
-class Agere_Wysiwyg_Model_Observer extends Varien_Event_Observer {
-
-	public function addWysiwygHandler(Varien_Event_Observer $observer) {
-		/** @var Mage_Catalog_Helper_Output $_helperOutput */
-		/** @var Agere_Wysiwyg_Helper_Filter $_helperFilter */
-		$_helperOutput = Mage::helper('catalog/output');
-		$_helperFilter = Mage::helper('agere_wysiwyg/filter');
-		$_helperOutput->addHandler('categoryAttribute', $_helperFilter);
-		$_helperOutput->addHandler('productAttribute', $_helperFilter);
-	}
-
+class Agere_Wysiwyg_Model_Observer extends Varien_Event_Observer
+{
+    public function addWysiwygHandler(Varien_Event_Observer $observer)
+    {
+        /** @var Mage_Catalog_Helper_Output $_helperOutput */
+        /** @var Agere_Wysiwyg_Helper_Filter $_helperFilter */
+        $_helperOutput = Mage::helper('catalog/output');
+        $_helperFilter = Mage::helper('agere_wysiwyg/filter');
+        $_helperOutput->addHandler('categoryAttribute', $_helperFilter);
+        $_helperOutput->addHandler('productAttribute', $_helperFilter);
+    }
 }
